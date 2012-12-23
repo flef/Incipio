0dad57130d6bd1f1c56b466a844607fc805738d2
Alexandre Brun <alexandrebrun.brun@gmail.com>
2012-12-23 15:30:48 +0100

    Possibilité d'ajout de Convention Client depuis une étude

--- src/mgate/SuiviBundle/Controller/CcController.php
+++ src/mgate/SuiviBundle/Controller/CcController.php
@@ -25,46 +25,10 @@ class CcController extends Controller
         ));
          
     }  
-    public function addAction()
+    public function addAction($id)
     {
         
         
-        $cc = new Cc;
-
-        $form        = $this->createForm(new CcType, $cc);
-        $formHandler = new CcHandler($form, $this->get('request'), $this->getDoctrine()->getEntityManager());
-
-        if($formHandler->process())
-        {
-            return $this->redirect( $this->generateUrl('mgateSuivi_etude_voir', array('id' => $cc->getId())) );
-        }
-
-        return $this->render('mgateSuiviBundle:Etude:ajouter.html.twig', array(
-            'form' => $form->createView(),
-        ));
-        
-    }
-    
-    public function voirAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('mgateSuiviBundle:Etude')->find($id); // Ligne qui posse problème
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Etude entity.');
-        }
-
-        //$deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('mgateSuiviBundle:Etude:voir.html.twig', array(
-            'etude'      => $entity,
-            /*'delete_form' => $deleteForm->createView(),  */      ));
-        
-    }
-    
-    public function modifierAction($id)
-    {
         $em = $this->getDoctrine()->getEntityManager();
 
         // On vérifie que l'article d'id $id existe bien, sinon, erreur 404.
@@ -72,19 +36,76 @@ class CcController extends Controller
         {
             throw $this->createNotFoundException('Article[id='.$id.'] inexistant');
         }
-
-        // On passe l'$article récupéré au formulaire
-        $form        = $this->createForm(new EtudeType, $etude);
-        $formHandler = new EtudeHandler($form, $this->get('request'), $em);
-
+        
+        
+        $cc = new Cc;
+        $cc->setEtude($etude);
+        $form        = $this->createForm(new CcType, $cc);
+        $formHandler = new CcHandler($form, $this->get('request'), $em);
+        
         if($formHandler->process())
         {
-            return $this->redirect( $this->generateUrl('mgateSuivi_etude_voir', array('id' => $etude->getId())) );
+            if($this->get('request')->get('next'))
+            {
+               
+                return $this->redirect($this->generateUrl('mgateSuivi_suivi_ajouter',array('id' => $etude->getId())));
+            }
+            else
+            {
+                return $this->redirect( $this->generateUrl('mgateSuivi_etude_voir', array('id' => $etude->getId())) );
+            }
         }
 
-        return $this->render('mgateSuiviBundle:Etude:modifier.html.twig', array(
+        return $this->render('mgateSuiviBundle:Cc:ajouter.html.twig', array(
             'form' => $form->createView(),
-            'etude' => $etude,
+        ));
+        
+    }
+    
+    public function voirAction($id)
+    {
+          $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('mgateSuiviBundle:Cc')->find($id); // Ligne qui posse problème
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Cc entity.');
+        }
+
+        //$deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('mgateSuiviBundle:Cc:voir.html.twig', array(
+            'cc'      => $entity,
+            /*'delete_form' => $deleteForm->createView(),  */      ));
+        
+    }
+    
+    public function modifierAction($id)
+    {
+        
+        $em = $this->getDoctrine()->getEntityManager();
+
+        if( ! $cc = $em->getRepository('mgate\SuiviBundle\Entity\Cc')->find($id) )
+        {
+            throw $this->createNotFoundException('Cc[id='.$id.'] inexistant');
+        }
+
+        $form        = $this->createForm(new CcType, $cc);
+        
+        if( $this->get('request')->getMethod() == 'POST' )
+        {
+            $form->bindRequest($this->get('request'));
+               
+            if( $form->isValid() )
+            {
+                return $this->redirect( $this->generateUrl('mgateSuivi_cc_voir', array('id' => $cc->getId())) );
+            }
+                
+        }
+
+        return $this->render('mgateSuiviBundle:Cc:modifier.html.twig', array(
+            'form' => $form->createView(),
+            'cc' => $cc,
         ));
     }
 }
