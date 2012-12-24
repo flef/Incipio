53fa219054220950a964f447fe0f98810cf6e76b
Alexandre Brun <alexandrebrun.brun@gmail.com>
2012-12-24 22:27:36 +0100

    Amélioration de l'AP (ajout de champ)

--- src/mgate/SuiviBundle/Entity/Ap.php
+++ src/mgate/SuiviBundle/Entity/Ap.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace mgate\SuiviBundle\Entity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -34,6 +35,35 @@ class Ap extends DocType
      */
     private $fraisDossier;
 
+    /**
+     * @var text $capacitedev
+     *
+     * @ORM\Column(name="capacitedev", type="text",nullable=true)
+     */
+    private $capacitedev;
+    
+     /**
+     * @var text $presentationprojet
+     *
+     * @ORM\Column(name="presentationprojet", type="text",nullable=true)
+     */
+    private $presentationprojet;
+    
+     /**
+     * @var text $descriptionprestation
+     *
+     * @ORM\Column(name="descriptionprestation", type="text",nullable=true)
+     */
+    private $descriptionprestation;
+    
+     /**
+     * @var integer $typePrestation
+     *
+     * @ORM\Column(name="validation", type="integer", nullable=true)
+     * @Assert\Choice(callback = "getValidationChoiceAssert")
+     */
+    private $typePrestation;
+    
 
     /**
      * Get id
@@ -89,5 +119,106 @@ class Ap extends DocType
     public function getEtude()
     {
         return $this->etude;
+    }
+
+    /**
+     * Set capacitedev
+     *
+     * @param string $capacitedev
+     * @return Ap
+     */
+    public function setCapacitedev($capacitedev)
+    {
+        $this->capacitedev = $capacitedev;
+    
+        return $this;
+    }
+
+    /**
+     * Get capacitedev
+     *
+     * @return string 
+     */
+    public function getCapacitedev()
+    {
+        return $this->capacitedev;
+    }
+
+    /**
+     * Set presentationprojet
+     *
+     * @param string $presentationprojet
+     * @return Ap
+     */
+    public function setPresentationprojet($presentationprojet)
+    {
+        $this->presentationprojet = $presentationprojet;
+    
+        return $this;
+    }
+
+    /**
+     * Get presentationprojet
+     *
+     * @return string 
+     */
+    public function getPresentationprojet()
+    {
+        return $this->presentationprojet;
+    }
+
+    /**
+     * Set descriptionprestation
+     *
+     * @param string $descriptionprestation
+     * @return Ap
+     */
+    public function setDescriptionprestation($descriptionprestation)
+    {
+        $this->descriptionprestation = $descriptionprestation;
+    
+        return $this;
+    }
+
+    /**
+     * Get descriptionprestation
+     *
+     * @return string 
+     */
+    public function getDescriptionprestation()
+    {
+        return $this->descriptionprestation;
+    }
+
+    /**
+     * Set typePrestation
+     *
+     * @param integer $typePrestation
+     * @return Ap
+     */
+    public function setTypePrestation($typePrestation)
+    {
+        $this->typePrestation = $typePrestation;
+    
+        return $this;
+    }
+
+    /**
+     * Get typePrestation
+     *
+     * @return integer 
+     */
+    public function getTypePrestation()
+    {
+        return $this->typePrestation;
+    }
+    
+    public static function getValidationChoice()
+    {
+        return array( 0 => "ingénieur informatique", 1 => "ingénieur électronique", 2 => "ingénieur informatique et électronique", 3=>"ingénieur microélectronique");
+    }
+    public static function getValidationChoiceAssert()
+    {
+        return array_keys(Ap::getValidationChoice());
     }
 }
