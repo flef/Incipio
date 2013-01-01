b6166697bdc09a4c1996421652e80e280374198e
Alexandre Brun <alexandrebrun.brun@gmail.com>
2013-01-01 18:44:11 +0100

    Résolution de conflit 2.

--- src/mgate/SuiviBundle/Controller/ApController.php
+++ src/mgate/SuiviBundle/Controller/ApController.php
@@ -14,7 +14,6 @@ use mgate\SuiviBundle\Entity\Prospect;
 use mgate\SuiviBundle\Entity\Personne;
 use mgate\SuiviBundle\Entity\Employe;
 
-use mgate\PersonneBundle\Entity\Employe;
 
 use mgate\SuiviBundle\Form\ApType;
 use mgate\SuiviBundle\Form\ApHandler;
