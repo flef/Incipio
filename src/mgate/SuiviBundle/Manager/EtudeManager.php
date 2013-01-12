2b38964da0d8017a1e6d4aac71cede0b6c54e10a
Florian Lefevre <florianlefevre@live.fr>
2013-01-12 20:20:34 +0100

    Modification Publipostage

--- src/mgate/SuiviBundle/Manager/EtudeManager.php
+++ src/mgate/SuiviBundle/Manager/EtudeManager.php
@@ -6,25 +6,23 @@ use Doctrine\ORM\EntityManager;
 use mgate\SuiviBundle\Manager\BaseManager;
 use mgate\SuiviBundle\Entity\Etude as Etude;
 
-class EtudeManager extends \Twig_Extension
-{
+class EtudeManager extends \Twig_Extension {
+
     protected $em;
     protected $tva;
 
-    public function __construct(EntityManager $em, $tva)
-    {
+    public function __construct(EntityManager $em, $tva) {
         $this->em = $em;
         $this->tva = $tva;
     }
-    
+
     // Pour utiliser les fonctions depuis twig
-    public function getName()
-    {
+    public function getName() {
         return 'mgate_EtudeManager';
     }
+
     // Pour utiliser les fonctions depuis twig
-    public function getFunctions()
-    {
+    public function getFunctions() {
         return array(
             'getRefEtude' => new \Twig_Function_Method($this, 'getRefEtude')
         );
@@ -33,88 +31,99 @@ class EtudeManager extends \Twig_Extension
     /**
      * Get montant total HT
      */
-    public function getTotalJEHHT(Etude $etude)
-    {
-        $total=0;
+    public function getTotalJEHHT(Etude $etude) {
+        $total = 0;
         foreach ($etude->getPhases() as $phase) {
-            $total += $phase->getNbrJEH()*$phase->getPrixJEH();
+            $total += $phase->getNbrJEH() * $phase->getPrixJEH();
         }
-        
+
         return $total;
     }
+
     /**
      * Get montant total HT
      */
-    public function getTotalHT(Etude $etude)
-    {
-        $total = $etude->getFraisDossier()+$this->getTotalJEHHT($etude);
-        
+    public function getTotalHT(Etude $etude) {
+        $total = $etude->getFraisDossier() + $this->getTotalJEHHT($etude);
+
         return $total;
     }
-        
+
     /**
      * Get montant total TTC
      */
-    public function getTotalTTC(Etude $etude)
-    {      
-        return $this->getTotalHT($etude)*(1+$this->tva);
+    public function getTotalTTC(Etude $etude) {
+        return $this->getTotalHT($etude) * (1 + $this->tva);
     }
-    
+
     /**
      * Get nombre de JEH
      */
-    public function getNbrJEH(Etude $etude)
-    {
+    public function getNbrJEH(Etude $etude) {
         $total = 0;
-        
+
         foreach ($etude->getPhases() as $phase) {
             $total += $phase->getPrixJEH();
         }
-        
+
         return $total;
-    }  
-    
+    }
+
     /**
      * Get référence de l'etude
      */
-    public function getRefEtude(Etude $etude)
-    {      
-        return "[M-GaTE]".(string)($etude->getMandat()*100+$etude->getNum());
+    public function getRefEtude(Etude $etude) {
+        return "[M-GaTE]" . (string) ($etude->getMandat() * 100 + $etude->getNum());
     }
-    
+
     /**
      * Get référence document
      */
-    public function getRefDoc(Etude $etude, $doc, $version)
-    {      
-        return $this->getRefEtude($etude)."-".$doc."-".$version; //TODO faire les autres type de docs, genre RM
+    public function getRefDoc(Etude $etude, $doc, $version) {
+        return $this->getRefEtude($etude) . "-" . $doc . "-" . $version; //TODO faire les autres type de docs, genre RM
     }
-    
+
     /**
      * Get nouveau numéro d'etude, pour valeur par defaut dans formulaire
      */
-    public function getNouveauNumero($mandat=5)
-    {      
+    public function getNouveauNumero($mandat = 5) {
         $qb = $this->em->createQueryBuilder();
-        
+
         $query = $qb->select('e.num')
-                   ->from('mgateSuiviBundle:Etude', 'e')
-                   ->andWhere('e.mandat = :mandat')
-                       ->setParameter('mandat', $mandat)
-                   ->orderBy('e.num', 'DESC');
-                    
-        $value=$query->getQuery()->setMaxResults(1)->getOneOrNullResult();
-        if($value)
-            return $value['num']+1;
+                ->from('mgateSuiviBundle:Etude', 'e')
+                ->andWhere('e.mandat = :mandat')
+                ->setParameter('mandat', $mandat)
+                ->orderBy('e.num', 'DESC');
+
+        $value = $query->getQuery()->setMaxResults(1)->getOneOrNullResult();
+        if ($value)
+            return $value['num'] + 1;
         else
             return 1;
     }
-    
-   
-    
 
-    public function getRepository()
-    {
+    public function getDateLancement(Etude $etude) {
+        $dateDebut = array();
+        $phases = $etude->getPhases();
+
+        foreach ($phases as $phase)
+            array_push($dateDebut, $phase->getDateDebut());
+        
+        return min($dateDebut);
+    }
+
+    public function getDateFin(Etude $etude) {
+        $dateFin = array();
+        $phases = $etude->getPhases();
+
+        foreach ($phases as $phase) {
+            $dateDebut = $phase->getDateDebut();
+            array_push($dateFin, $dateDebut->modify('+'.$phase->getDelai() . 'day'));
+        }
+        return max($dateFin);
+    }
+
+    public function getRepository() {
         return $this->em->getRepository('mgateSuiviBundle:Etude');
     }
 
