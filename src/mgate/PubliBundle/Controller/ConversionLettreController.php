0ad6e20fc13453784ce5c6a5d0a108e7a76e4a6c
Florian Lefevre <florianlefevre@live.fr>
2013-01-12 20:51:22 +0100

    Modification ConvertNombre

--- src/mgate/PubliBundle/Controller/ConversionLettreController.php
+++ src/mgate/PubliBundle/Controller/ConversionLettreController.php
@@ -4,7 +4,6 @@ namespace mgate\PubliBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 /**
  * Description of chiffreEnLettre
  * source : http://www.javascriptfr.com/codes/CONVERSION-CHIFFRE-MONETAIRE-LETTRE_30141.aspx
@@ -12,202 +11,210 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * Conversion limitée à 999 999 999 999 999 ou 9 999 999 999 999,99
  * si le nombre contient plus de 2 décimales, il est arrondit à 2 décimales
  */
+class ConversionLettreController extends Controller {
 
-class ConversionLettreController extends Controller
-{
     /**
-    * fonction permettant de transformer une valeur numérique en valeur en lettre
-    * @param int $Nombre le nombre a convertir
-    * @param int $Devise (0 = aucune, 1 = Euro €, 2 = Dollar $)
-    * @param int $Langue (0 = Français, 1 = Belgique, 2 = Suisse)
-    * @return string la chaine
-    */
-    public function ConvNumberLetter($Nombre, $Devise=0, $Langue=0) {
-        $dblEnt=''; $byDec='';
-        $bNegatif='';
+     * fonction permettant de transformer une valeur numérique en valeur en lettre
+     * @param int $Nombre le nombre a convertir
+     * @param int $Devise (0 = aucune, 1 = Euro €, 2 = Dollar $)
+     * @param int $Langue (0 = Français, 1 = Belgique, 2 = Suisse)
+     * @return string la chaine
+     */
+    public function ConvNumberLetter($Nombre, $Devise = 0, $Langue = 0) {
+        $dblEnt = '';
+        $byDec = '';
+        $bNegatif = '';
         $strDev = '';
         $strCentimes = '';
 
-        if( $Nombre < 0 ) {
+        if ($Nombre < 0) {
             $bNegatif = true;
             $Nombre = abs($Nombre);
-
         }
-        $dblEnt = intval($Nombre) ;
-            $byDec = round(($Nombre - $dblEnt) * 100) ;
-        if( $byDec == 0 ) {
+        $dblEnt = intval($Nombre);
+        $byDec = round(($Nombre - $dblEnt) * 100);
+        if ($byDec == 0) {
             if ($dblEnt > 999999999999999) {
-                return "#TropGrand" ;
+                return "#TropGrand";
             }
-            }
-        else {
+        } else {
             if ($dblEnt > 9999999999999.99) {
-                return "#TropGrand" ;
+                return "#TropGrand";
             }
-            }
-            switch($Devise) {
+        }
+        switch ($Devise) {
             case 0 :
-                if ($byDec > 0) $strDev = " virgule" ;
-                            break;
+                if ($byDec > 0)
+                    $strDev = " virgule";
+                break;
             case 1 :
-                $strDev = " euro" ;
-                if ($byDec > 0) $strCentimes = $strCentimes . " centime" . ($byDec > 1 ? 's' : '');
-                            break;
+                $strDev = " euro";
+                if ($byDec > 0)
+                    $strCentimes = $strCentimes . " centime" . ($byDec > 1 ? 's' : '');
+                break;
             case 2 :
-                $strDev = " dollar" ;
-                if ($byDec > 0) $strCentimes = $strCentimes . " cent" ;
-                            break;
-            }
-        if (($dblEnt > 1) && ($Devise != 0)) $strDev .= "s" ;
-        if (($dblEnt > 1) && ($Devise != 0)) $strDev .= " et" ;
-            $NumberLetter = $this->ConvNumEnt(floatval($dblEnt), $Langue) . $strDev . " " . $this->ConvNumDizaine($byDec, $Langue) . $strCentimes ;
-            return $NumberLetter;
+                $strDev = " dollar";
+                if ($byDec > 0)
+                    $strCentimes = $strCentimes . " cent";
+                break;
+        }
+        if (($dblEnt > 1) && ($Devise != 0))
+            $strDev .= "s";
+        if (($byDec > 0) && ($Devise != 0))
+            $strDev .= " et";
+        $NumberLetter = $this->ConvNumEnt(floatval($dblEnt), $Langue) . $strDev;
+        if (($byDec > 0) && ($Devise != 0))
+            $NumberLetter .= " " . $this->ConvNumDizaine($byDec, $Langue) . $strCentimes;
+        return $NumberLetter;
     }
 
     private function ConvNumEnt($Nombre, $Langue) {
-       $byNum=$iTmp=$dblReste='' ;
-       $StrTmp = '';
-       $NumEnt='' ;
-        $iTmp = $Nombre - (intval($Nombre / 1000) * 1000) ;
-        $NumEnt = $this->ConvNumCent(intval($iTmp), $Langue) ;
-        $dblReste = intval($Nombre / 1000) ;
-        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000) ;
-        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue) ;
-        switch($iTmp) {
+        $byNum = $iTmp = $dblReste = '';
+        $StrTmp = '';
+        $NumEnt = '';
+        $iTmp = $Nombre - (intval($Nombre / 1000) * 1000);
+        $NumEnt = $this->ConvNumCent(intval($iTmp), $Langue);
+        $dblReste = intval($Nombre / 1000);
+        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000);
+        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue);
+        switch ($iTmp) {
             case 0 :
-                            break;
+                break;
             case 1 :
-                $StrTmp = "mille " ;
-                            break;
+                $StrTmp = "mille ";
+                break;
             default :
-                $StrTmp = $StrTmp . " mille " ;
+                $StrTmp = $StrTmp . " mille ";
         }
-        $NumEnt = $StrTmp . $NumEnt ;
-        $dblReste = intval($dblReste / 1000) ;
-        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000) ;
-        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue) ;
-        switch($iTmp) {
+        $NumEnt = $StrTmp . $NumEnt;
+        $dblReste = intval($dblReste / 1000);
+        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000);
+        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue);
+        switch ($iTmp) {
             case 0 :
-                            break;
+                break;
             case 1 :
-                $StrTmp = $StrTmp . " million " ;
-                            break;
+                $StrTmp = $StrTmp . " million ";
+                break;
             default :
-                $StrTmp = $StrTmp . " millions " ;
+                $StrTmp = $StrTmp . " millions ";
         }
-        $NumEnt = $StrTmp . $NumEnt ;
-        $dblReste = intval($dblReste / 1000) ;
-        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000) ;
-        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue) ;
-            switch($iTmp) {
+        $NumEnt = $StrTmp . $NumEnt;
+        $dblReste = intval($dblReste / 1000);
+        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000);
+        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue);
+        switch ($iTmp) {
             case 0 :
-                            break;
+                break;
             case 1 :
-                $StrTmp = $StrTmp . " milliard " ;
-                            break;
+                $StrTmp = $StrTmp . " milliard ";
+                break;
             default :
-                $StrTmp = $StrTmp . " milliards " ;
+                $StrTmp = $StrTmp . " milliards ";
         }
-        $NumEnt = $StrTmp . $NumEnt ;
-        $dblReste = intval($dblReste / 1000) ;
-        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000) ;
-        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue) ;
-            switch($iTmp) {
+        $NumEnt = $StrTmp . $NumEnt;
+        $dblReste = intval($dblReste / 1000);
+        $iTmp = $dblReste - (intval($dblReste / 1000) * 1000);
+        $StrTmp = $this->ConvNumCent(intval($iTmp), $Langue);
+        switch ($iTmp) {
             case 0 :
-                            break;
+                break;
             case 1 :
-                $StrTmp = $StrTmp . " billion " ;
-                            break;
+                $StrTmp = $StrTmp . " billion ";
+                break;
             default :
-                $StrTmp = $StrTmp . " billions " ;
+                $StrTmp = $StrTmp . " billions ";
         }
-        $NumEnt = $StrTmp . $NumEnt ;
+        $NumEnt = $StrTmp . $NumEnt;
         return $NumEnt;
     }
 
     private function ConvNumDizaine($Nombre, $Langue) {
-        $TabUnit=$TabDiz='';
-        $byUnit=$byDiz='' ;
-        $strLiaison = '' ;
+        $TabUnit = $TabDiz = '';
+        $byUnit = $byDiz = '';
+        $strLiaison = '';
 
         $TabUnit = array("", "un", "deux", "trois", "quatre", "cinq", "six", "sept",
             "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze",
-            "seize", "dix-sept", "dix-huit", "dix-neuf") ;
+            "seize", "dix-sept", "dix-huit", "dix-neuf");
         $TabDiz = array("", "", "vingt", "trente", "quarante", "cinquante",
-            "soixante", "soixante", "quatre-vingt", "quatre-vingt") ;
+            "soixante", "soixante", "quatre-vingt", "quatre-vingt");
         if ($Langue == 1) {
-            $TabDiz[7] = "septante" ;
-            $TabDiz[9] = "nonante" ;
-            }
-        else if ($Langue == 2) {
-            $TabDiz[7] = "septante" ;
-            $TabDiz[8] = "huitante" ;
-            $TabDiz[9] = "nonante" ;
+            $TabDiz[7] = "septante";
+            $TabDiz[9] = "nonante";
+        } else if ($Langue == 2) {
+            $TabDiz[7] = "septante";
+            $TabDiz[8] = "huitante";
+            $TabDiz[9] = "nonante";
         }
-        $byDiz = intval($Nombre / 10) ;
-        $byUnit = $Nombre - ($byDiz * 10) ;
-        $strLiaison = "-" ;
-        if ($byUnit == 1) $strLiaison = " et " ;
-        switch($byDiz) {
+        $byDiz = intval($Nombre / 10);
+        $byUnit = $Nombre - ($byDiz * 10);
+        $strLiaison = "-";
+        if ($byUnit == 1)
+            $strLiaison = " et ";
+        switch ($byDiz) {
             case 0 :
-                $strLiaison = "" ;
-                            break;
+                $strLiaison = "";
+                break;
             case 1 :
-                $byUnit = $byUnit + 10 ;
-                $strLiaison = "" ;
-                            break;
+                $byUnit = $byUnit + 10;
+                $strLiaison = "";
+                break;
             case 7 :
-                if ($Langue == 0) $byUnit = $byUnit + 10 ;
-                            break;
+                if ($Langue == 0)
+                    $byUnit = $byUnit + 10;
+                break;
             case 8 :
-                if ($Langue != 2) $strLiaison = "-" ;
-                            break;
+                if ($Langue != 2)
+                    $strLiaison = "-";
+                break;
             case 9 :
                 if ($Langue == 0) {
-                    $byUnit = $byUnit + 10 ;
-                    $strLiaison = "-" ;
+                    $byUnit = $byUnit + 10;
+                    $strLiaison = "-";
                 }
-                            break;
+                break;
         }
-        $NumDizaine = $TabDiz[$byDiz] ;
-        if ($byDiz == 8 && $Langue != 2 && $byUnit == 0) $NumDizaine = $NumDizaine . "s" ;
+        $NumDizaine = $TabDiz[$byDiz];
+        if ($byDiz == 8 && $Langue != 2 && $byUnit == 0)
+            $NumDizaine = $NumDizaine . "s";
         if ($TabUnit[$byUnit] != "") {
-            $NumDizaine = $NumDizaine . $strLiaison . $TabUnit[$byUnit] ;
-            }
-        else {
-            $NumDizaine = $NumDizaine ;
+            $NumDizaine = $NumDizaine . $strLiaison . $TabUnit[$byUnit];
+        } else {
+            $NumDizaine = $NumDizaine;
         }
         return $NumDizaine;
     }
 
     private function ConvNumCent($Nombre, $Langue) {
-        $TabUnit='' ;
-        $byCent=$byReste='' ;
-        $strReste = '' ;
-        $NumCent='';
-        $TabUnit = array("", "un", "deux", "trois", "quatre", "cinq", "six", "sept","huit", "neuf", "dix") ;
+        $TabUnit = '';
+        $byCent = $byReste = '';
+        $strReste = '';
+        $NumCent = '';
+        $TabUnit = array("", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix");
 
-        $byCent = intval($Nombre / 100) ;
-        $byReste = $Nombre - ($byCent * 100) ;
+        $byCent = intval($Nombre / 100);
+        $byReste = $Nombre - ($byCent * 100);
         $strReste = $this->ConvNumDizaine($byReste, $Langue);
-        switch($byCent) {
+        switch ($byCent) {
             case 0 :
-                $NumCent = $strReste ;
-                            break;
+                $NumCent = $strReste;
+                break;
             case 1 :
                 if ($byReste == 0)
-                    $NumCent = "cent" ;
+                    $NumCent = "cent";
                 else
-                    $NumCent = "cent " . $strReste ;
+                    $NumCent = "cent " . $strReste;
                 break;
             default :
                 if ($byReste == 0)
-                    $NumCent = $TabUnit[$byCent] . " cents" ;
+                    $NumCent = $TabUnit[$byCent] . " cents";
                 else
-                    $NumCent = $TabUnit[$byCent] . " cent " . $strReste ;
-            }
-            return $NumCent;
+                    $NumCent = $TabUnit[$byCent] . " cent " . $strReste;
+        }
+        return $NumCent;
     }
+
 }
 
 ?>
