b52a85394bfbf20448622d4fbb6d513d4098e4cf
Stephane Collot <stephane.collot@gmail.com>
2013-02-02 18:10:06 +0100

    ajout du super utilisateur propre

--- src/mgate/PersonneBundle/DataFixtures/ORM/LoadUserData.php
+++ src/mgate/PersonneBundle/DataFixtures/ORM/LoadUserData.php
@@ -12,16 +12,6 @@ class LoadUserData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $su = new Personne();
-        $su->setNom('admin');
-        $su->setPrenom('test');
-        $su->setAdresse('bbbb');
-        $su->setEmail('wtf@hh.fr');
-        $su->setSexe('m');
-      
-        
-
-        $manager->persist($su);
-        $manager->flush();
+        //Osef
     }
 }
