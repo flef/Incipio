a1aa404d339cb400177cf28ac3d8fb88944455a4
Stephane Collot <stephane.collot@gmail.com>
2013-02-02 16:13:24 +0100

    debut des indicateurs

--- src/mgate/StatBundle/Controller/DefaultController.php
+++ src/mgate/StatBundle/Controller/DefaultController.php
@@ -4,10 +4,45 @@ namespace mgate\StatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Ob\HighchartsBundle\Highcharts\Highchart;
+
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('mgateStatBundle:Default:index.html.twig', array('name' => $name));
+        // Chart
+        $series = array(
+            array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+        $ob->title->text('Chart Title');
+        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->series($series);
+
+        return $this->render('mgateStatBundle:Default:test.html.twig', array(
+            'chart' => $ob
+        ));
+    }
+    
+    public function caAction()
+    {
+        // Chart
+        $series = array(
+            array("name" => "Chiffre d'Affaire total",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+        $ob->title->text('Chiffre d\'Affaire titre du graph');
+        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->series($series);
+
+        return $this->render('mgateStatBundle:Default:ca.html.twig', array(
+            'chart' => $ob
+        ));
     }
 }
