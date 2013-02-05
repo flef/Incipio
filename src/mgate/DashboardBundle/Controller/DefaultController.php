e0a8542ed274553a3b33986ac4950661fafe01bf
Stephane Collot <stephane.collot@gmail.com>
2013-02-05 20:31:18 +0100

    ok les mail marchent

--- src/mgate/DashboardBundle/Controller/DefaultController.php
+++ src/mgate/DashboardBundle/Controller/DefaultController.php
@@ -7,18 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction()
-    {
-            $message = \Swift_Message::newInstance()
-        ->setSubject('Hello Email')
-        ->setFrom('mgate.je@gmail.com')
-        ->setTo('stephane.collot@gmail.com')
-        ->setBody("test" )
-    ;
-    $this->get('mailer')->send($message);
-    
-    echo "loool";
-        
-        
+    {        
         return $this->render('mgateDashboardBundle:Default:index.html.twig');
     }
 }
