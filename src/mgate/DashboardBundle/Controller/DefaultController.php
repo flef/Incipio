74434d9bd6ff4fc1507026c8bbbfa046aef8775c
Stephane Collot <stephane.collot@gmail.com>
2013-02-05 20:28:44 +0100

    test mail

--- src/mgate/DashboardBundle/Controller/DefaultController.php
+++ src/mgate/DashboardBundle/Controller/DefaultController.php
@@ -8,6 +8,17 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+            $message = \Swift_Message::newInstance()
+        ->setSubject('Hello Email')
+        ->setFrom('mgate.je@gmail.com')
+        ->setTo('stephane.collot@gmail.com')
+        ->setBody("test" )
+    ;
+    $this->get('mailer')->send($message);
+    
+    echo "loool";
+        
+        
         return $this->render('mgateDashboardBundle:Default:index.html.twig');
     }
 }
