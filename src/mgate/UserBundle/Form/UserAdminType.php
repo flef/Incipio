50aee1fbdb0d0c112b49ff6545b367294250df44
Stephane Collot <stephane.collot@gmail.com>
2013-02-06 17:45:26 +0100

    gestion des droits

---
+++ src/mgate/UserBundle/Form/UserAdminType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace mgate\UserBundle\Form;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+use mgate\PersonneBundle\Form\PersonneType as PersonneType;
+use mgate\PersonneBundle\Entity\PersonneRepository as PersonneRepository;
+use mgate\UserBundle\Entity\User as User;
+
+class UserAdminType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('personne', 'entity', 
+                array ('label' => 'Séléctionner la personne',
+                       'class' => 'mgate\\PersonneBundle\\Entity\\Personne',
+                       'property' => 'prenomNom',
+                       'property_path' => true,
+                       'required' => false,
+                       //'query_builder' => function(PersonneRepository $pr) { return $pr->getNotUser(); },
+                               
+                               ));   
+                       
+       $builder->add('roles', 'choice', array(
+        'choices' => User::getRolesNames(),
+        'required' => false,'label'=>'Roles','multiple'=>true
+    ));   
+                       
+        parent::buildForm($builder, $options);
+        
+    }
+
+    public function getName()
+    {
+        return 'mgate_user_useradmin';
+    }
+    
+
+    
+}
