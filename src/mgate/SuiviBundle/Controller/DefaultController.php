d753f1aef2e1de496d019c14b48bbef66526b075
Stephane Collot <stephane.collot@gmail.com>
2013-02-16 15:50:30 +0100

    modification du suivi de l'AP en ajax

--- src/mgate/SuiviBundle/Controller/DefaultController.php
+++ src/mgate/SuiviBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace mgate\SuiviBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\HttpFoundation\Response;
+
+use mgate\SuiviBundle\Entity\Ap;
+use mgate\SuiviBundle\Form\DocTypeSuiviType;
 
 class DefaultController extends Controller
 {
