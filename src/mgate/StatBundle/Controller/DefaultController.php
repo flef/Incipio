52596e30c02e31aa14581ba0068a17473d6538b0
Stephane Collot <stephane.collot@gmail.com>
2013-03-12 23:56:11 +0100

    amelioration stat CA signé

--- src/mgate/StatBundle/Controller/DefaultController.php
+++ src/mgate/StatBundle/Controller/DefaultController.php
@@ -38,23 +38,26 @@ class DefaultController extends Controller
      */    
     public function caAction()
     {
+        $etudeManager = $this->get('mgate.etude_manager');
         $em = $this->getDoctrine()->getManager();
         $etude = new \mgate\SuiviBundle\Entity\Etude;
         $etudes = $em->getRepository('mgateSuiviBundle:Etude')->getEtudesCa();
         
-        $ca = array();
+        $data = array();
         $cumul=0;
         foreach ($etudes as $etude) {
-            echo "bite";
+
             if($etude->getCc())
             {
                 if($etude->getCc()->getDateSignature() )
                 {
-                    $cumul+= $this->get('mgate.etude_manager')->getTotalHT($etude);
-                    $value = array();
-                    $value[]=$etude->getCc()->getDateSignature()->getTimestamp()*1000;
-                    $value[]=$cumul;
-                    $ca[] = $value;
+                    $cumul+= $etudeManager->getTotalHT($etude);
+                    
+                    $data[]= array( "x"=>$etude->getCc()->getDateSignature()->getTimestamp()*1000,
+                                    "y"=>$cumul, "name"=>$etudeManager->getRefEtude($etude)." - ".$etude->getNom(),
+                                    'date'=>$etude->getCc()->getDateSignature()->format('d/m/Y'),
+                                    'prix'=>$etudeManager->getTotalHT($etude));
+                    
                 }
             }
        }
@@ -63,16 +66,18 @@ class DefaultController extends Controller
         
         // Chart
         $series = array(
-            array("name" => "Chiffre d'Affaire total",    "data" => $ca)
+            array("name" => "Chiffre d'Affaire total",    "data" => $data)
         );
 
         $ob = new Highchart();
         $ob->global->useUTC(false);
         $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
         $ob->title->text('Chiffre d\'Affaire titre du graph');
-        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->xAxis->title(array('text'  => "Date"));
         $ob->xAxis->type('datetime');
-        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->yAxis->title(array('text'  => "Chiffre d'Affaire cumulé"));
+        $ob->tooltip->headerFormat('<b>{series.name}</b><br />');
+        $ob->tooltip->pointFormat('{point.y} le {point.date}<br />{point.name} à {point.prix} €');
         $ob->series($series);
 
         return $this->render('mgateStatBundle:Default:ca.html.twig', array(
