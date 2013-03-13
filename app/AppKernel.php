735644a5deda9a6ebee75b737b57ccd140effb01
Florian Lefevre <florianlefevre@live.fr>
2013-03-14 00:00:43 +0100

    Bundle Navbar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new mgate\DashboardBundle\mgateDashboardBundle(),
             new mgate\StatBundle\mgateStatBundle(),
+            new mgate\NavbarBundle\NavbarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
