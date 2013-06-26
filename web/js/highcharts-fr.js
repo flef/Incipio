91a4df06a12e27b460147a128a4895726f82749d
Stephane Collot <stephane.collot@gmail.com>
2013-06-26 09:53:23 +0200

    oublie des fichiers JS

---
+++ web/js/highcharts-fr.js
@@ -0,0 +1,20 @@
+Highcharts.setOptions({
+lang: {
+months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+weekdays: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+shortMonths: ['Jan', 'Fév', 'Mars', 'Avr', 'Mai', 'Juin',
+'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'],
+decimalPoint: ",",
+downloadPNG: "Télécharger en image PNG",
+downloadJPEG: "Télécharger en image JPEG",
+downloadPDF: "Télécharger en document PDF",
+downloadSVG: "Télécharger en document Vectoriel",
+exportButtonTitle: "Export du graphique",
+loading: "Chargement en cours...",
+printButtonTitle: "Imprimer le graphique",
+resetZoom: "Réinitialiser le zoom",
+resetZoomTitle: "Réinitialiser le zoom au niveau 1:1",
+thousandsSep: " "
+}
+});
