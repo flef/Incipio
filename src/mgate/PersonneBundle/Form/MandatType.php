086906dad98ef0b7b6629d3c87d02f8e4be0cbea
Florian Lefevre <lefevreflorian@live.fr>
2013-07-07 01:22:18 +0200

    Modification controller + form Add/Modif Membre pour les nouveaux mandats/postes

---
+++ src/mgate/PersonneBundle/Form/MandatType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace mgate\PersonneBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class MandatType extends AbstractType
+{
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+    {
+	    $builder
+                ->add('debutMandat', 'genemu_jquerydate', array('label'=>'Date de début', 'format'=>'d/MM/y', 'required'=>false, 'widget'=>'single_text'))
+                ->add('finMandat', 'genemu_jquerydate', array('label'=>'Date de Fin', 'format'=>'d/MM/y', 'required'=>false, 'widget'=>'single_text'))
+                ->add('poste', 'entity', 
+                    array ('label' => 'Séléctionner un poste',
+                           'class' => 'mgate\\PersonneBundle\\Entity\\Poste',
+                           'property' => 'intitule',
+                           'property_path' => true,
+                           'required' => false,));
+                    
+          
+    }
+
+    public function getName()
+    {
+        return 'mgate_personnebundle_mandatetype';
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'mgate\PersonneBundle\Entity\Mandat',
+        );
+    }
+}
+
