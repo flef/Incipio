19792df1099e98f70e46a6a86dc0f0e7fd93928a
Florian Lefevre <lefevreflorian@live.fr>
2013-07-12 21:01:53 +0200

    Début de gestion des listes de diffusion + obtention des adresses etu selon Promo ou adresses emse-ismin si ancien

--- src/mgate/PersonneBundle/Controller/PersonneController.php
+++ src/mgate/PersonneBundle/Controller/PersonneController.php
@@ -18,9 +18,43 @@ class PersonneController extends Controller {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('mgatePersonneBundle:Personne')->findAll();
-
+        
         return $this->render('mgatePersonneBundle:Personne:annuaire.html.twig', array(
                     'personnes' => $entities,
                 ));
     }
+    
+   /**
+     * @Secure(roles="ROLE_SUIVEUR")
+     */
+    public function listeMailAction(){
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('mgatePersonneBundle:Personne')->findBy(array('estAbonneNewsletter' => true, 'emailEstValide' => true));
+        
+        
+        return $this->render('mgatePersonneBundle:Personne:listeDiffusion.html.twig', array(
+                    'personnes' => $entities,
+                    
+                ));
+    }
+
+    /**
+     * @Secure(roles="ROLE_ADMIN")
+     */    
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+   
+           if( ! $entity = $em->getRepository('mgate\PersonneBundle\Entity\Personne')->find($id) )
+                throw $this->createNotFoundException('Prospect[id='.$id.'] inexistant');
+            
+            $em->remove($entity);
+            $em->flush();
+        
+        return $this->redirect($this->generateUrl('mgatePersonne_annuaire'));
+    }
+    
+    
+    
 }
