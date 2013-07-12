186a4c1f46d826bcf30252b1eb8fdaafc5dac48c
Florian Lefevre <lefevreflorian@live.fr>
2013-07-13 00:09:44 +0200

    Debug

--- src/mgate/PersonneBundle/Entity/Membre.php
+++ src/mgate/PersonneBundle/Entity/Membre.php
@@ -94,7 +94,7 @@ class Membre {
             if ($promo < $now)
                 return preg_replace('#[^a-zA-Z.0-9_]#','',$this->enMinusculeSansAccent($this->getPersonne()->getPrenom() . $this->getPersonne()->getNom())) . "@emse-ismin.fr";
             if ($promo < 2014)
-                return preg_replace('#[^a-zA-Z.0-9_]','',$this->enMinusculeSansAccent(substr($this->getPersonne ()->getPrenom (), 0, 1) . $this->getPersonne ()->getNom ())) . "@etu.emse.fr";
+                return preg_replace('#[^a-zA-Z.0-9_]#','',$this->enMinusculeSansAccent(substr($this->getPersonne ()->getPrenom (), 0, 1) . $this->getPersonne ()->getNom ())) . "@etu.emse.fr";
             else
             return preg_replace('#[^a-zA-Z.0-9_]#','',$this->enMinusculeSansAccent($this->getPersonne()->getPrenom() . '.' . $this->getPersonne()->getNom())) . "@etu.emse.fr";
         }
