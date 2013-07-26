50716a95435222eca0e29e387062ae5615efbe06
Florian Lefevre <florianlefevre@live.fr>
2013-07-26 17:27:44 +0200

    Modification graphique + Categorie + Formulaire

--- src/mgate/FormationBundle/Form/FormationType.php
+++ src/mgate/FormationBundle/Form/FormationType.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace mgate\FormationBundle\Form;
+
 use mgate\FormationBundle\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
@@ -10,40 +11,40 @@ use mgate\PersonneBundle\Entity\PersonneRepository as PersonneRepository;
 class FormationType extends AbstractType {
 
     public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options) {
-        $builder->add('titre', 'text', array('label' => 'Présentation du projet', 'required' => false,))
+        $builder->add('titre', 'text', array('label' => 'Titre de la formation', 'required' => false,))
                 ->add('description', 'textarea', array('label' => 'Description de la Formation', 'required' => false,))
                 ->add('categorie', 'choice', array('choices' => Formation::getCategoriesChoice(), 'label' => 'Catégorie', 'required' => false))
-                ->add('dateDebut', 'genemu_jquerydate', array('label' => 'Date de debut', 'format' => 'd/MM/y', 'required' => false, 'widget' => 'single_text'))
-                ->add('dateFin', 'genemu_jquerydate', array('label' => 'Date de fin', 'format' => 'd/MM/y', 'required' => false, 'widget' => 'single_text'))
+                ->add('dateDebut', 'datetime', array('label' => 'Date de debut (d/MM/y - HH:mm:ss)', 'format' => 'd/MM/y - HH:mm:ss', 'required' => false, 'widget' => 'single_text'))
+                ->add('dateFin', 'datetime', array('label' => 'Date de fin (d/MM/y - HH:mm:ss)', 'format' => 'd/MM/y - HH:mm:ss', 'required' => false, 'widget' => 'single_text'))
                 ->add('formateurs', 'collection', array(
-                        'type' => 'genemu_jqueryselect2_entity', 
+                    'type' => 'genemu_jqueryselect2_entity',
                     'options' => array('label' => 'Suiveur de projet',
-                    'class' => 'mgate\\PersonneBundle\\Entity\\Personne',
-                    'property' => 'prenomNom',
-                    'property_path' => true,
-                    'query_builder' => function(PersonneRepository $pr) {
-                        return $pr->getMembreOnly();
-                    },
-                    'required' => false),
-                        'allow_add' => true,
-                        'allow_delete' => true,
-                        'by_reference' => false,
-                    ))
-                            ->add('membresPresents', 'collection', array(
-                        'type' => 'genemu_jqueryselect2_entity', 
+                        'class' => 'mgate\\PersonneBundle\\Entity\\Personne',
+                        'property' => 'prenomNom',
+                        'property_path' => true,
+                        'query_builder' => function(PersonneRepository $pr) {
+                            return $pr->getMembreOnly();
+                        },
+                        'required' => false),
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                ))
+                ->add('membresPresents', 'collection', array(
+                    'type' => 'genemu_jqueryselect2_entity',
                     'options' => array('label' => 'Suiveur de projet',
-                    'class' => 'mgate\\PersonneBundle\\Entity\\Personne',
-                    'property' => 'prenomNom',
-                    'property_path' => true,
-                    'query_builder' => function(PersonneRepository $pr) {
-                        return $pr->getMembreOnly();
-                    },
-                    'required' => false),
-                        'allow_add' => true,
-                        'allow_delete' => true,
-                        'by_reference' => false,
-                    ))
-                ;
+                        'class' => 'mgate\\PersonneBundle\\Entity\\Personne',
+                        'property' => 'prenomNom',
+                        'property_path' => true,
+                        'query_builder' => function(PersonneRepository $pr) {
+                            return $pr->getMembreOnly();
+                        },
+                        'required' => false),
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                ))
+        ;
     }
 
     public function getName() {
