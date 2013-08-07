2079f8015200244d38b2d109e4f36fdf04b6d9f6
Florian Lefevre <florianlefevre@live.fr>
2013-08-07 14:24:59 +0200

    Add in Form Type : use Symfony\Component\OptionsResolver\OptionsResolverInterface;

--- src/mgate/SuiviBundle/Form/CommentaireSuiviType.php
+++ src/mgate/SuiviBundle/Form/CommentaireSuiviType.php
@@ -3,6 +3,7 @@
 namespace mgate\SuiviBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
 use mgate\SuiviBundle\Entity\Etude;
