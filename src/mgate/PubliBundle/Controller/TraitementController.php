36995045e400993f7973d010fa2678babba822e4
Florian Lefevre <florianlefevre@live.fr>
2013-11-20 13:10:32 +0100

    Reduction de code

--- src/mgate/PubliBundle/Controller/TraitementController.php
+++ src/mgate/PubliBundle/Controller/TraitementController.php
@@ -183,51 +183,8 @@ class TraitementController extends Controller {
     }
 
     private function nombreVersMois($m) {
-
-        $m %= 12;
-
-        $mois = NULL;
-        switch ($m) {
-            case 1:
-                $mois = 'janvier';
-                break;
-            case 2:
-                $mois = 'février';
-                break;
-            case 3:
-                $mois = 'mars';
-                break;
-            case 4:
-                $mois = 'avril';
-                break;
-            case 5:
-                $mois = 'mai';
-                break;
-            case 6:
-                $mois = 'juin';
-                break;
-            case 7:
-                $mois = 'juillet';
-                break;
-            case 8:
-                $mois = 'août';
-                break;
-            case 9:
-                $mois = 'septembre';
-                break;
-            case 10:
-                $mois = 'octobre';
-                break;
-            case 11:
-                $mois = 'novembre';
-                break;
-            case 12:
-                $mois = 'décembre';
-                break;
-            default:
-                break;
-        }
-        return $mois;
+        $mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+        return $mois[($m % 12) - 1];
     }
 
     private function getAllChamp($etude, $doc, $key = 0) {
