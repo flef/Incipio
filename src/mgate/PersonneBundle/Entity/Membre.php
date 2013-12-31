7364d2c0a0c88bf9576426b90a1271f5218d166b
Florian Lefevre <florianlefevre@live.fr>
2013-12-31 03:25:50 +0100

    Listing des intervenants

--- src/mgate/PersonneBundle/Entity/Membre.php
+++ src/mgate/PersonneBundle/Entity/Membre.php
@@ -21,6 +21,11 @@ class Membre {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="mgate\SuiviBundle\Entity\Mission", mappedBy="intervenant", cascade={"persist","remove"})
+     */
+    private $missions;
 
     /**
      * @ORM\OneToOne(targetEntity="mgate\PersonneBundle\Entity\Personne", inversedBy="membre", cascade={"persist", "merge", "remove"})
@@ -208,6 +213,7 @@ class Membre {
      */
     public function __construct() {
         $this->mandats = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->missions = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
     /**
@@ -392,5 +398,38 @@ class Membre {
     public function getDateConventionEleve()
     {
         return $this->dateConventionEleve;
+    }
+
+    /**
+     * Add missions
+     *
+     * @param \mgate\SuiviBundle\Entity\Mission $missions
+     * @return Membre
+     */
+    public function addMission(\mgate\SuiviBundle\Entity\Mission $missions)
+    {
+        $this->missions[] = $missions;
+    
+        return $this;
+    }
+
+    /**
+     * Remove missions
+     *
+     * @param \mgate\SuiviBundle\Entity\Mission $missions
+     */
+    public function removeMission(\mgate\SuiviBundle\Entity\Mission $missions)
+    {
+        $this->missions->removeElement($missions);
+    }
+
+    /**
+     * Get missions
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMissions()
+    {
+        return $this->missions;
     }
 }
