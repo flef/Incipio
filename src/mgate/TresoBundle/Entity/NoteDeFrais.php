9b20a08f4a2f8624bc59fb61fc031879ec03119d
Florian Lefevre <florianlefevre@live.fr>
2014-01-01 23:48:07 +0100

    Gestion des Notes de Frais : Class

---
+++ src/mgate/TresoBundle/Entity/NoteDeFrais.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace mgate\TresoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use mgate\SuiviBundle\Entity\DocType as DocType;
+
+/**
+ * NoteDeFrais
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class NoteDeFrais extends DocType
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @var integer $mandat
+     *
+     * @ORM\Column(name="mandat", type="integer", nullable=false)
+     */
+    private $mandat;
+
+    /**
+     * @var integer $num
+     *
+     * @ORM\Column(name="num", type="integer", nullable=false)
+     */
+    private $numero;
+    
+    /**
+     * @ORM\Column(name="objet", type="text", nullable=false)
+     * @var string
+     */
+    private $objet;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="NoteDeFraisDetail", mappedBy="noteDeFrais", cascade={"persist", "remove"})
+     */
+    private $details;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="mgate\PersonneBundle\Entity\Personne")
+     */
+    protected $demandeur;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->details = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Set objet
+     *
+     * @param string $objet
+     * @return NoteDeFrais
+     */
+    public function setObjet($objet)
+    {
+        $this->objet = $objet;
+    
+        return $this;
+    }
+
+    /**
+     * Get objet
+     *
+     * @return string 
+     */
+    public function getObjet()
+    {
+        return $this->objet;
+    }
+
+    /**
+     * Add details
+     *
+     * @param \mgate\TresoBundle\Entity\NoteDeFraisDetail $details
+     * @return NoteDeFrais
+     */
+    public function addDetail(\mgate\TresoBundle\Entity\NoteDeFraisDetail $details)
+    {
+        $this->details[] = $details;
+    
+        return $this;
+    }
+
+    /**
+     * Remove details
+     *
+     * @param \mgate\TresoBundle\Entity\NoteDeFraisDetail $details
+     */
+    public function removeDetail(\mgate\TresoBundle\Entity\NoteDeFraisDetail $details)
+    {
+        $this->details->removeElement($details);
+    }
+
+    /**
+     * Get details
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * Set demandeur
+     *
+     * @param \mgate\PersonneBundle\Entity\Personne $demandeur
+     * @return NoteDeFrais
+     */
+    public function setDemandeur(\mgate\PersonneBundle\Entity\Personne $demandeur = null)
+    {
+        $this->demandeur = $demandeur;
+    
+        return $this;
+    }
+
+    /**
+     * Get demandeur
+     *
+     * @return \mgate\PersonneBundle\Entity\Personne 
+     */
+    public function getDemandeur()
+    {
+        return $this->demandeur;
+    }
+
+    /**
+     * Set mandat
+     *
+     * @param integer $mandat
+     * @return NoteDeFrais
+     */
+    public function setMandat($mandat)
+    {
+        $this->mandat = $mandat;
+    
+        return $this;
+    }
+
+    /**
+     * Get mandat
+     *
+     * @return integer 
+     */
+    public function getMandat()
+    {
+        return $this->mandat;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     * @return NoteDeFrais
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return integer 
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+}
