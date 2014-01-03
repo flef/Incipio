c28b7c637b091747ece9ddc2c729ba19da94ec42
Florian Lefevre <florianlefevre@live.fr>
2014-01-03 02:26:44 +0100

    Ajout compte comptable pour catgoriser les NF

---
+++ src/mgate/TresoBundle/Controller/CompteController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace mgate\TresoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use mgate\TresoBundle\Entity\Compte;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use mgate\TresoBundle\Form\CompteType;
+
+class CompteController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $comptes = $em->getRepository('mgateTresoBundle:Compte')->findAll();
+        
+        return $this->render('mgateTresoBundle:Compte:index.html.twig', array('comptes' => $comptes));
+    }
+    
+    /**
+     * @Secure(roles="ROLE_CA")
+     */
+    public function modifierAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        if (!$compte= $em->getRepository('mgateTresoBundle:Compte')->find($id)) {
+            $compte = new Compte;            
+        }
+
+        $form = $this->createForm(new CompteType, $compte);
+
+        if( $this->get('request')->getMethod() == 'POST' )
+        {
+            $form->bind($this->get('request'));
+            if( $form->isValid() )
+            {
+                $em->persist($compte);                
+                $em->flush();
+                
+                var_dump($compte);
+                return $this->redirect($this->generateUrl('mgateTreso_Compte_index', array()));
+            }
+        }
+
+        return $this->render('mgateTresoBundle:Compte:modifier.html.twig', array(
+                    'form' => $form->createView(),
+                    'compte' =>$compte,
+                ));
+    }
+    
+    /**
+     * @Secure(roles="ROLE_ADMIN")
+     */
+    public function supprimerAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        if (!$compte= $em->getRepository('mgateTresoBundle:Compte')->find($id))
+            throw $this->createNotFoundException('Le Compte n\'existe pas !');
+
+        $em->remove($compte);                
+        $em->flush();
+        return $this->redirect($this->generateUrl('mgateTreso_Compte_index', array()));
+
+
+    }
+    
+    
+    
+    
+    
+    
+}
